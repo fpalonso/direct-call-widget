609564d0ee08b32e3622382542ef5acf270a2e71
Fer PA <blax.software@gmail.com>
2025-01-27 00:00:46 +0100

    Migrate to versions catalog (#52)
    * Also, migrate project and :app module build.gradle files to Kotlin

---
+++ core/analytics/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.library")
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "dev.ferp.dcw.core.analytics"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    flavorDimensions += "analytics"
+    productFlavors {
+        create("collectionEnabled") {
+            dimension = "analytics"
+            resValue("bool", "collect_analytics", "true")
+        }
+        create("collectionDisabled") {
+            dimension = "analytics"
+            resValue("bool", "collect_analytics", "false")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Analytics
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics.ktx)
+}
