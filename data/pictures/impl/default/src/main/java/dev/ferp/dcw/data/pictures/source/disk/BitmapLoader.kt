4346b60298596f6b3b4767d9849fad5841105ddd
Fer PA <blax.software@gmail.com>
2025-02-01 14:43:30 +0100

    Move /data/pictures/data/* to /data/pictures/

--- data/pictures/impl/default/src/main/java/dev/ferp/dcw/data/pictures/source/disk/BitmapLoader.kt
+++ data/pictures/impl/default/src/main/java/dev/ferp/dcw/data/pictures/source/disk/BitmapLoader.kt
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package dev.ferp.dcw.data.pictures.data.source.disk
+package dev.ferp.dcw.data.pictures.source.disk
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
--- data/pictures/impl/default/src/test/java/dev/ferp/dcw/data/pictures/source/disk/BitmapLoaderTest.kt
+++ data/pictures/impl/default/src/test/java/dev/ferp/dcw/data/pictures/source/disk/BitmapLoaderTest.kt
@@ -1,8 +1,25 @@
-package dev.ferp.dcw.data.pictures.data.source.disk
+/*
+ * Direct Call Widget - The widget that makes contacts accessible
+ * Copyright (C) 2025 Fer P. A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.ferp.dcw.data.pictures.source.disk
 
 import android.graphics.Bitmap
 import com.bumptech.glide.load.engine.GlideException
-import dev.ferp.dcw.data.pictures.data.source.disk.BitmapLoader.ContinuationRequestListener
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Before
@@ -14,12 +31,12 @@ import kotlin.coroutines.resumeWithException
 class BitmapLoaderTest {
 
     private lateinit var continuation: Continuation<Bitmap>
-    private lateinit var requestListener: ContinuationRequestListener
+    private lateinit var requestListener: BitmapLoader.ContinuationRequestListener
 
     @Before
     fun setUp() {
         continuation = mockk(relaxed = true)
-        requestListener = ContinuationRequestListener(continuation)
+        requestListener = BitmapLoader.ContinuationRequestListener(continuation)
     }
 
     @Test
