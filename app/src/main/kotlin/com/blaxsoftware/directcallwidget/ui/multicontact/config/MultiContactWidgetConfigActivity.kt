11d3bd6b2b5fab9999027fc0851758966b5854aa
Fer PA <blax.software@gmail.com>
2026-08-02 16:12:49 +0200

    Handle AppWidgetId correctly in MultiContactWidgetConfigActivity

--- app/src/main/kotlin/com/blaxsoftware/directcallwidget/ui/multicontact/config/MultiContactWidgetConfigActivity.kt
+++ app/src/main/kotlin/com/blaxsoftware/directcallwidget/ui/multicontact/config/MultiContactWidgetConfigActivity.kt
@@ -18,6 +18,8 @@
 
 package com.blaxsoftware.directcallwidget.ui.multicontact.config
 
+import android.appwidget.AppWidgetManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -33,14 +35,29 @@ class MultiContactWidgetConfigActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setResult(RESULT_CANCELED)
+
+        val appWidgetId = intent?.extras?.getInt(
+            AppWidgetManager.EXTRA_APPWIDGET_ID,
+            AppWidgetManager.INVALID_APPWIDGET_ID
+        ) ?: AppWidgetManager.INVALID_APPWIDGET_ID
+
+        val resultValue = Intent().apply {
+            putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        }
+        setResult(RESULT_CANCELED, resultValue)
+
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+            return
+        }
+
         setContent {
             DirectCallWidgetTheme {
                 Scaffold { paddingValues ->
                     MultiContactNavGraph(
                         modifier = Modifier.padding(paddingValues),
                         onSaveClick = {
-                            setResult(RESULT_OK)
+                            setResult(RESULT_OK, resultValue)
                             finish()
                         }
                     )
