ec7f2d245833ba3c498ff7814ba74e0ec97b410f
Fer PA <blax.software@gmail.com>
2026-08-02 15:47:48 +0200

    Fix Java 25 compilation (#100)
    * Fix Java 25 compilation issues by upgrading Gradle to 9.5.1 and updating OSS licenses plugin
    
    * Update CI to Java 25 and adjust Robolectric SDK level to 35
    
    * Remove unused import

--- app/src/test/kotlin/com/blaxsoftware/directcallwidget/legacy/LegacyWidgetsTest.kt
+++ app/src/test/kotlin/com/blaxsoftware/directcallwidget/legacy/LegacyWidgetsTest.kt
@@ -3,7 +3,6 @@ package com.blaxsoftware.directcallwidget.legacy
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.blaxsoftware.directcallwidget.appwidget.DirectCallWidgetProvider
 import com.blaxsoftware.directcallwidget.appwidget.DirectCallWidgetProvider1x1
 import com.blaxsoftware.directcallwidget.findByComponent
@@ -26,7 +25,7 @@ import org.robolectric.shadows.ShadowApplication
 
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class LegacyWidgetsTest {
 
     @get:Rule
